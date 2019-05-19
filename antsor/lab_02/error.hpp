a8a39eb1e3615da2e9f0ab29c925538d81de2277
SGCube <sortech-sorgamer@yandex.ru>
2019-05-19 12:51:37 +0300

    lab_02_a: error improve (time, file, class)

--- antsor/lab_02/error.hpp
+++ antsor/lab_02/error.hpp
@@ -3,57 +3,78 @@
 
 #include <exception>
 #include <string>
+#include <chrono>
+#include <cstring>
+#include <sstream>
 
-using std::exception;
-using std::string;
-
-class Error : public exception
+class Error : public std::exception
 {
 public:
-    Error();
-    explicit Error(const char* msg) : message(msg) {}
-    explicit Error(const string& msg) : message(msg) {}
+    Error(std::chrono::time_point<std::chrono::system_clock> time,
+          std::string fileName, std::string className, std::string method, const std::string& msg):
+            timePoint(time),
+            filename(std::move(fileName)),
+            classname(std::move(className)),
+            method(std::move(method),
+            message(std::move(msg));
 
     virtual const char* what() const noexcept override
     {
-        return message.c_str();
+        return errorMsg().c_str();
     }
 
 protected:
-    string message;
+    std::chrono::time_point<std::chrono::system_clock> timePoint;
+    std::string filename;
+    std::string classname;
+    std::string method;
+    std::string message;
+
+    virtual std::string errorMsg() const
+    {
+        auto time = std::chrono::system_clock::to_time_t(timePoint);
+        std::stringstream s;
+        s << time;
+        return "List error!" +
+               "\nTime: " + s.str() +
+               "\nFile: " + fileName +
+               "\nClass: " + className +
+               "\nMethod: " + method +
+               "\nError: " + message;
+    }
 };
 
 class ErrorMemory : public Error
 {
 public:
-    explicit ErrorMemory() : Error(messageError) {}
-    explicit ErrorMemory(const string& msg) : Error(messageError + msg) {}
-
+    explicit ErrorMemory(std::chrono::time_point<std::chrono::system_clock> time,
+                         std::string fileName, std::string className,
+                         std::string method) :
+                         Error(time, fileName, className, method, msg) {}
 private:
-    static constexpr const char* messageError = "Memory allocation error!";
-    static constexpr const char* messageErrorExt = "Memory allocation error: ";
+    static constexpr const char* msg = "Memory allocation error";
 };
 
-class ErrorEmpty : public Error
+class ErrorEmptyList : public Error
 {
 public:
-    explicit ErrorEmpty() : Error(messageError) {}
-    explicit ErrorEmpty(const string& msg) : Error(messageError + msg) {}
-
+    explicit ErrorMemory(std::chrono::time_point<std::chrono::system_clock> time,
+                         std::string fileName, std::string className,
+                         std::string method) :
+                         Error(time, fileName, className, method, msg) {}
 private:
-    static constexpr const char* messageError = "List is empty!";
-    static constexpr const char* messageErrorExt = "List is empty: ";
+    static constexpr const char* msg = "List is empty";
 };
 
-class ErrorListRange : public Error
+class ErrorOutOfListRange : public Error
 {
 public:
-    explicit ErrorListRange() : Error(messageError) {}
-    explicit ErrorListRange(const string& msg) : Error(messageError + msg) {}
-
+    explicit ErrorMemory(std::chrono::time_point<std::chrono::system_clock> time,
+                         std::string fileName, std::string className,
+                         std::string method) :
+                         Error(time, fileName, className, method, msg) {}
 private:
-    static constexpr const char* messageError = "Index out of range!";
-    static constexpr const char* messageErrorExt = "Index out of range: ";
+    static constexpr const char* msg = "Out of list range";
 };
 
 #endif //ERROR_HPP
